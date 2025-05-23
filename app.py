fb08bab659c21940d856db76de59db1b9280d0e4
Nico <nmcava01@gmail.com>
2025-05-23 12:24:41 -0400

    added step 6

--- app.py
+++ app.py
@@ -17,12 +17,19 @@
 # create a .gitignore file, specify files you want git to ignore, such as the venv folder
 
 #! STEP 5:
+# Upload files to GitHub
 # git init
 # git add .
 # git commit -m "commit message"
 # git branch -M main
 # git remote add origin <github_repository_url>
 # git push -u origin main
+
+#! STEP 6:
+# Go to render.com and sign in
+# Go to Web Service
+# Connect your git repository from GitHub to Render
+# Follow the steps of deployment
 
 from flask import Flask
 
